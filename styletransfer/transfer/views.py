da7e2809950468d03d8973022827e35fcb939e23
Leejeongsueng1 <jki1256@hanmail.net>
2022-05-24 14:44:12 +0900

    파일 업로드

--- styletransfer/transfer/views.py
+++ styletransfer/transfer/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 import os
+from PIL import Image
 image_base_path = './static/img'
 
 def index(request):
     if request.method == 'POST':
-        target_image = request.FILES.getlist('img')
+        target_image = Image.open(request.FILES["target_img"])
         style_image = request.POST['style_img']
+        target_image.show()
         #combination_image = style_transfer(target_image,style_image)
         print(target_image)
-        target_path = os.path.join(image_base_path , target_image)
-        style_path = os.path.join(image_base_path , style_image)
-
+        target_path = os.path.join(image_base_path , str(target_image))
+        style_path = os.path.join(image_base_path , str(style_image))
+        print(target_path,style_path)
         context = {'target_image':target_image,
                     'style_image':style_image}
                     #'combination':combination_image}
