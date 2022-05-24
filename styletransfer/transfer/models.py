018d9ea343fd16aa99a2a1edab2e4bdf169decc0
Leejeongsueng1 <jki1256@hanmail.net>
2022-05-24 15:49:24 +0900

    베이스 템플릿 추가

--- styletransfer/transfer/models.py
+++ styletransfer/transfer/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Document(models.Model):
     title = models.CharField(max_length = 200)
-    uploadedFile = models.FileField(upload_to = "Uploaded Files/")
+    uploadedFile = models.FileField(upload_to = "assets/")
     dateTimeOfUpload = models.DateTimeField(auto_now = True)
