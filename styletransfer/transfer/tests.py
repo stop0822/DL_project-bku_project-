5b248ede69d94728aa894c51ff97a420778fd277
Leejeongsueng1 <jki1256@hanmail.net>
2022-05-25 10:08:05 +0900

    static change

--- styletransfer/transfer/tests.py
+++ styletransfer/transfer/tests.py
@@ -1,7 +1,5 @@
-from sqlite3 import Time
-from django.test import TestCase
+from PIL import Image
 
-# Create your tests here.
-import time
 
-print(type(time.localtime().tm_year) , time.localtime().tm_mon , time.localtime().tm_mday )
+picture  = Image.open('transfer/static/img/')
+
