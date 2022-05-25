0d501fe410515e68a2b58494124b0843adc76584
Leejeongsueng1 <jki1256@hanmail.net>
2022-05-25 12:56:09 +0900

    파일 경로 수정 및 views.py 업데이트

--- styletransfer/transfer/tests.py
+++ styletransfer/transfer/tests.py
@@ -1,5 +1,10 @@
 from PIL import Image
+import os
 
 
-picture  = Image.open('static/img/Manet.jpg')
-picture.show()
+#picture  = Image.open('static/img/Manet.jpg')
+#picture.show()
+Path = os.getcwd()
+print(Path)
+print(os.listdir('assets/20225252620/'))
+
--- styletransfer/transfer/views.py
+++ styletransfer/transfer/views.py
@@ -14,18 +14,24 @@ def index(request):
         style_image = request.POST["style_img"]
         file_prefix = str(localtime().tm_year ) + str(localtime().tm_mon) + str(localtime().tm_mday) + str(localtime().tm_min) + str(localtime().tm_sec) 
         os.mkdir(target_image_base_path + file_prefix)
-        target_image.show()
         target_image.save(target_image_base_path + file_prefix +'/target.jpg')
         target_path = target_image_base_path + file_prefix +'/target.jpg'
         
         style_path = style_image_base_path + style_image + '.jpg'
-        combination_image = crystalize(target_path, style_path, target_image_base_path + file_prefix)
+        crystalize(target_path, style_path, target_image_base_path + file_prefix)
+        combination_image = os.listdir('assets/' + file_prefix + '/')
+        
+
+
         print(target_path,style_path)
         context = {'target_image':target_image,
-                    'style_image':style_image}
-                    #'combination':combination_image}
+                    'style_image':style_image,
+                    'combination':combination_image}
         return render(request,'main.html', context)
 
     if request.method == 'GET':
+        combination_image = os.listdir('assets/' + file_prefix + '/')
+        context = {
+                    'combination':combination_image}
 
-        return render(request,'main.html', {})
+        return render(request,'main.html', context)
